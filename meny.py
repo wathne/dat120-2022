eb96fb8cd127bcec4f48d73682b268b0181eaa55
Joar Wathne <wathne@users.noreply.github.com>
2022-10-31 14:07:27 +0100

    Add(Issue l): meny.py

---
+++ meny.py
@@ -0,0 +1,24 @@
+"""Klasser og funksjoner for et menysystem.
+"""
+
+
+# TODO(Issue l): Lag et menysystem.
+def menysystem() -> None:
+    """Starter et menysystem.
+    
+    Args:
+
+    Returns:
+
+    Raises:
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    pass
+    # TEST: menysystem().
+    print(menysystem())
+
+
